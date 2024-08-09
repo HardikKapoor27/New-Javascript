11cb662c0470ec7cab7ca4375a363e75fefebabe
HardikKapoor27 <161691002+HardikKapoor27@users.noreply.github.com>
2024-08-09 17:12:27 +0000

    new file:   New-Javascript/01_Variables.js deleted:    New-Javascript/Test.js

---
+++ New-Javascript/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "hardik27@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gwalior"
+
+accountEmail = "hk@gmail.com"
+accountPassword = "54321"
+accountCity = "New Delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
