fa275b89c3a619714c135d38e133c8350637ea40
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2024-08-13 16:12:34 +0000

    Conversion-Operation

---
+++ New-Javascript/03_conversion-operation.js
@@ -0,0 +1,11 @@
+let score = "33"
+let score2 = "33"
+let score3 = "33"
+let score4 = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
