62be54bab5f4155a9a0d892992a5f372f1f15620
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2024-09-22 14:25:40 +0000

    Changes

--- New-Javascript/datatypes-summary.js
+++ New-Javascript/datatypes-summary.js
@@ -33,3 +33,20 @@ const myFunction = function() {
 } // Function
 
 console.log(typeof bigNumber);
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
