ff31104e60fdd4d37fddc9ebc5c57af9a23eab6b
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2024-09-22 14:23:59 +0000

    Datatype-Summary

---
+++ New-Javascript/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Types Of Datatypes :
+
+// #1 Primitive 
+// 7 Types : String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const name = "Hardik"  // String
+const score = 100 // Number
+const scoreValue = 100.33 // Number
+const isLoggedIn = false // Boolean
+const outsideTemp = null // Null
+let userEmail; let userEmail2 = undefined // undefined
+
+const id = Symbol('123') // Symbol
+const anotherId = Symbol('123') // Symbol
+
+console.log(id === anotherId); 
+const bigNumber = 3456543576654356754n // BigInt
+
+// #2 Reference (Non-primitive)
+// 3 Types : Array,Objects,Functions
+
+const supercars = ["Buggati","Ferrari","Lamborgini","Porsche","BMW","Mercedes","Konisegg"]; // Array
+
+let myObj = {
+    
+    name: "Hardik",
+    age: 21,
+
+} // Object
+
+const myFunction = function() {
+    console.log("Hello World");
+} // Function
+
+console.log(typeof bigNumber);
